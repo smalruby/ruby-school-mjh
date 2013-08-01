5d64e30936507bec2a10d994b62ded37ca01a1c1
Kouji Takao <kouji@netlab.jp>
2013-08-01 09:22:06 +0900

    .

--- mac/kadai/no2/no2.rb
+++ mac/kadai/no2/no2.rb
@@ -2,40 +2,69 @@
 
 require 'r2d'
 
-window width: 640, height: 480
+window
 
-title = Text.new(10, 10, 30, "数当てゲーム,0~9の数をあてよう！")
-explain = Text.new(30, 40, 30, "Rを押したら答えをリセット")
-answer = Text.new(100, 200, 75, "", 'red')
+N = 10
+X = 30
+Y = 400
+W = 35
+H = 50
+
+Text.new(10, 10, 30, "数当てゲーム,0~#{N - 1}の数をあてよう！", 'green')
+Text.new(10, 40, 25, '数字をクリック,Rキーで答えをリセット')
 
 t = []
-10.times do|i|
-	t.push(Text.new(30 + 35*i, 400, 50, "#{i}" ,'white'))
+N.times do |i|
+  t << Text.new(X + W * i, Y, H, i.to_s ,'white')
 end
 
-num = 0
-ans = rand(10)
+num = -1
+ans = rand(N)
+a = Text.new(25, 200, 75, '')
 
 key_down 'mouse_left' do
-	x = mouse_x
-	y = mouse_y
+  x = mouse_x
+  y = mouse_y
 
-	10.times do |i|
-		m_x = 35 * (i+1)
-		if (m_x<x && x<m_x+30) && (400<y && y<440)
-			num = i
-			answer.content = "#{num}よりも大きい" if ans > num
-			answer.content = "#{num}よりも小さい" if ans < num
-			answer.content = "正解は#{ans}" if ans == num
-			t[i].color = 'red'
-		else
-			t[i].color = 'white'
-		end
-	end
+  N.times do |i|
+    mx = W * (i + 1)
+    if mx < x && x < mx + X && Y < y && y < Y + H
+      num = i
+      break
+    end
+  end
+
+  if num == -1
+    next
+  end
+
+  N.times do |i|
+    if i == num
+      t[i].color = 'red'
+    else
+      t[i].color = 'white'
+    end
+  end
+
+  if ans > num
+    a.content = "#{num}よりも大きい"
+    a.color = 'yellow'
+  elsif ans < num
+    a.content = "#{num}よりも小さい"
+    a.color = 'blue'
+  else
+    a.content = '正解！！！'
+    a.color = 'green'
+  end
 end
 
 key_down 'r' do
-	ans = rand(10)
+  num = -1
+  ans = rand(N)
+  a.content = ''
+  N.times do |i|
+    t[i].color = 'white'
+  end
 end
 
 window :show
